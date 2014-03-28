b8a8c7494c9f872cb317b3210b66aa3176842b89
charxie <charxie@gmail.com>
2014-03-28 14:49:46 -0400

    test github

--- energy2d/src/org/concord/energy2d/system/System2D.java
+++ energy2d/src/org/concord/energy2d/system/System2D.java
@@ -106,7 +106,7 @@ public class System2D extends JApplet implements MwService, ManipulationListener
 
 	public System2D() {
 
-		// Locale.setDefault(Locale.US); for applets, this is a security violation
+		// Locale.setDefault(Locale.US); for the applet, this is a security violation
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
