28d6c666c9213316a9e0573cc053e62bc2b6bafa
charxie <charxie@gmail.com>
2015-08-28 14:46:03 -0400

    test eclipse mars

--- energy2d/src/org/concord/energy2d/system/System2D.java
+++ energy2d/src/org/concord/energy2d/system/System2D.java
@@ -77,6 +77,8 @@ import com.apple.eawt.ApplicationEvent;
 /**
  * Deploy as an app (energy2d.jar) or an applet (energy2d-applet.jar). The applet has no menu bar and tool bar and doesn't include /models and /resources.
  * 
+ * Note: Java applets do not work in Chrome any more.
+ * 
  * @author Charles Xie
  * 
  */
